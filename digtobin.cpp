4ba499534ef0f82478900093c1920359612a9cf4
maralexv <scarvesfamily@yahoo.com>
2021-07-19 20:59:50 +0200

    finished decimal to binary converter

--- digtobin.cpp
+++ digtobin.cpp
@@ -8,19 +8,23 @@ int main ()
     vector<int> result;
     cin >> given;
 
+    if (given == 0)
+    {
+        cout << 0 << endl;
+        return 0;
+    }
+
     while (given > 0) 
     {
         result.push_back(given % 2);
         given = given / 2;
     }
 
-    reverse(result.begin(), result.end());
-
-    for (int i = 0; i < result.size(); i++)
+    for (int i = result.size()-1; i >=0 ; i--) 
     {
-        cout << result[i];
+      cout << result[i];
     }
-    cout << endl;
 
+    cout << endl;
     return 0;
 }
