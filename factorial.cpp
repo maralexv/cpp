4796b3591bda0e4923e597096a9221f8ecbe411c
maralexv <scarvesfamily@yahoo.com>
2021-07-20 16:35:59 +0200

    new assignment palindrom

--- factorial.cpp
+++ factorial.cpp
@@ -2,24 +2,19 @@
 
 using namespace std;
 
-int Factorial(int x);
+int Factorial(int x) {
+  if (x > 0) {
+    x = x * Factorial(x - 1);
+  } else {
+    x = 1;
+  }
+  return x;
+}
 
 int main ()
 {
     int input;
     cin >> input;
-    cout << Factorial(input) << endl;
+    cout << Factorial (input) << endl;
     return 0;
-}
-
-int Factorial(int x) 
-{
-  if (x > 0) 
-  {
-    x = x * Factorial(x - 1);
-  } else 
-  {
-    x = 1;
-  }
-  return x;
 }
