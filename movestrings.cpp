82873d6c96cfb92efcfc2a5bdac709e06226c44d
maralexv <scarvesfamily@yahoo.com>
2021-07-23 15:56:40 +0200

    updated code for c++11

--- movestrings.cpp
+++ movestrings.cpp
@@ -2,13 +2,16 @@
 #include <vector>
 #include <string>
 using namespace std;
-using std::vector;
 
 void MoveStrings(vector<string>& source, vector<string>& destination)
 {
-    for (int i = 0; i < source.size(); i++)
+    // for (int i = 0; i < source.size(); i++)
+    // {
+    //     destination.push_back(source[i]);
+    // }
+    for (auto e : source) 
     {
-        destination.push_back(source[i]);
+      destination.push_back(e);
     }
     source.clear();    
 }
@@ -18,11 +21,13 @@ int main()
 {
     vector<string> sour = {"a", "b", "c"};
     vector<string> dest = {"d", "e", "f", "g"};
+
     MoveStrings(sour, dest);
     for (int i = 0; i < dest.size(); i++)
     {
-        cout << dest[i] << ", ";
+        cout << dest[i] << " ";
     }
     cout << endl;
+    
     return 0;
 }
